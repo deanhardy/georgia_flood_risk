452a4becc4d7594528dd7dcbeaaddc9caeba0f69
“Dean <deanhardy@gmail.com>
2020-11-04 14:00:01 -0500

    updating meridian graph with new data

--- scripts/gageheight_ml.R
+++ scripts/gageheight_ml.R
@@ -6,9 +6,9 @@ library(tidyverse) ## load tidyverse package
 ## set data directory
 datadir <- ("/Users/dhardy/Dropbox/r_data/georgia_hurricanes")
 
-## read in tidal data 
-df <- read.csv(file.path(datadir, "data/height_allobserved_ml.csv"), header=TRUE)
-df <- read.delim(file.path(datadir, "data/original/height_allobserved_ml.txt"), header=TRUE, sep = '\t', dec = '.',
+## read in high/low tidal data from gauge station 
+# df <- read.csv(file.path(datadir, "datsa/height_allobserved_ml.csv"), header=TRUE)
+df <- read.delim(file.path(datadir, "data/original/20201104_height_allobserved_ml.txt"), header=TRUE, sep = '\t', dec = '.',
                  skip = 29) %>%
   slice(2:n()) %>%
   rename(high = X35070_00065_00021, low = X35071_00065_00024,
@@ -31,7 +31,7 @@ lo10 <-
 fig <- ggplot(filter(df, type == 'high'), aes(datetime, height)) +
   geom_point(pch=19, size = 1, color = 'grey') + 
   geom_smooth(method = lm) + 
-  geom_hline(yintercept = 9.2, color = 'red') + 
+  geom_hline(yintercept = 9.5, color = 'red') + ## 9.5 feet is flood stage at Fort Pulaski
   scale_x_datetime(date_breaks = "1 years", date_labels = "%y") + 
   scale_y_continuous(breaks = c(0,1,2,3,4,5,6,7,8,9,10,11,12),
                      labels = c(0,1,2,3,4,5,6,7,8,9,10,11,12)) +
@@ -40,7 +40,8 @@ fig <- ggplot(filter(df, type == 'high'), aes(datetime, height)) +
   #ggtitle("Meridian Landing Gage") + 
   theme(axis.title = element_text(size = 20),
         axis.text = element_text(size = 18),
-        title = element_text(size = 20))
+        title = element_text(size = 20)) + 
+  annotate(geom="text", y = 9.5, x = df$datetime[100], label = "Fort Pulaski\nFlood Stage", col = 'red')
 fig
 
 tiff(file.path(datadir, 'figures/meridian_tides_alltime.tiff'), res=300, unit='in', width = 13.33, height = 7.0, 
