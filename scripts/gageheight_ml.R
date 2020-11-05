e4a7700a3bb9c9057a929f0dc4ef5f191732ba05
“Dean <deanhardy@gmail.com>
2020-11-05 12:19:51 -0500

    figure tweaks for pub

--- scripts/gageheight_ml.R
+++ scripts/gageheight_ml.R
@@ -28,23 +28,30 @@ lo10 <-
   filter(type == "low") %>%
   top_n(10, height)
 
+# lims <- as.POSIXct(strptime(c("2011-01-01 03:00","2011-01-01 16:00"), format = "%Y-%m-%d %H:%M"))    
+
 fig <- ggplot(filter(df, type == 'high'), aes(datetime, height)) +
-  geom_point(pch=19, size = 1, color = 'grey') + 
-  geom_smooth(method = lm) + 
-  geom_hline(yintercept = 9.5, color = 'red') + ## 9.5 feet is flood stage at Fort Pulaski
-  scale_x_datetime(date_breaks = "1 years", date_labels = "%y") + 
+  geom_point(pch=19, size = 0.1, color = 'grey') + 
+  geom_point(aes(datetime, height, col = 'red'), filter(df, type == 'high' & height >= 9.5), size = 1, pch = 17) + 
+  geom_smooth(method = lm, col = 'black', size = 0.5) + 
+  geom_hline(yintercept = 9.5, color = 'red', linetype = 'dashed') + ## 9.5 feet is flood stage at Fort Pulaski
+  scale_x_datetime(date_breaks = "2 years", date_labels = "%y", date_minor_breaks = '1 year',
+                   limits = c(df$datetime[1], df$datetime[14668]), expand = c(0,0)) + 
   scale_y_continuous(breaks = c(0,1,2,3,4,5,6,7,8,9,10,11,12),
                      labels = c(0,1,2,3,4,5,6,7,8,9,10,11,12)) +
   xlab("Year") +
-  ylab("High Tide Height (feet)") +
+  ylab("High Tide (feet)") +
   #ggtitle("Meridian Landing Gage") + 
-  theme(axis.title = element_text(size = 20),
-        axis.text = element_text(size = 18),
-        title = element_text(size = 20)) + 
-  annotate(geom="text", y = 9.5, x = df$datetime[100], label = "Fort Pulaski\nFlood Stage", col = 'red')
+  theme(axis.title = element_text(size = 10),
+        axis.text = element_text(size = 10),
+        title = element_text(size = 10),
+        panel.grid = element_blank(),
+        panel.background = element_rect(fill = 'white', color = 'black'),
+        legend.position = "none") + 
+  annotate(geom="text", y = 9.85, x = df$datetime[3700], label = "Fort Pulaski\nFlood Stage", col = 'red')
 fig
 
-tiff(file.path(datadir, 'figures/meridian_tides_alltime.tiff'), res=300, unit='in', width = 13.33, height = 7.0, 
+tiff(file.path(datadir, 'figures/meridian_tides_alltime.tiff'), res=300, unit='in', width = 6, height = 4, 
                compression = 'lzw')
 fig
 dev.off()
